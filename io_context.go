48ec0668cf88500d9bf8fe951de0a601c7a9706b
BubbaJoe <joe@Joes-Mac-mini.local>
2022-10-29 20:58:18 +0900

    extend IOContext to reader/writer/seeker

--- io_context.go
+++ io_context.go
@@ -3,11 +3,16 @@ package astiav
 //#cgo pkg-config: libavformat
 //#include <libavformat/avformat.h>
 import "C"
-import "unsafe"
+import (
+	"io"
+	"unsafe"
+)
 
 // https://github.com/FFmpeg/FFmpeg/blob/n5.0/libavformat/avio.h#L161
 type IOContext struct {
 	c *C.struct_AVIOContext
+	io.ReadWriteCloser
+	io.Seeker
 }
 
 func NewIOContext() *IOContext {
@@ -25,15 +30,36 @@ func (ic *IOContext) Closep() error {
 	return newError(C.avio_closep(&ic.c))
 }
 
+func (ic *IOContext) Close() error {
+	return newError(C.avio_close(ic.c))
+}
+
 func (ic *IOContext) Open(filename string, flags IOContextFlags) error {
 	cfi := C.CString(filename)
 	defer C.free(unsafe.Pointer(cfi))
 	return newError(C.avio_open(&ic.c, cfi, C.int(flags)))
 }
 
-func (ic *IOContext) Write(b []byte) {
-	if b == nil {
-		return
+func (ic *IOContext) Write(b []byte) (int, error) {
+	if len(b) == 0 {
+		return 0, nil
 	}
 	C.avio_write(ic.c, (*C.uchar)(unsafe.Pointer(&b[0])), C.int(len(b)))
+	return len(b), nil
+}
+
+func (ic *IOContext) Read(b []byte) (n int, err error) {
+	n = int(C.avio_read(ic.c, (*C.uchar)(unsafe.Pointer(&b[0])), C.int(len(b))))
+	if n < 0 {
+		return 0, io.EOF
+	}
+	return n, nil
+}
+
+func (ic *IOContext) Seekable() bool {
+	return int(ic.c.seekable) != 0
+}
+
+func (ic *IOContext) Seek(offset int64, whence int) (int64, error) {
+	return int64(C.avio_seek(ic.c, C.int64_t(offset), C.int(whence))), nil
 }
--- io_context_test.go
+++ io_context_test.go
@@ -1,27 +1,136 @@
 package astiav_test
 
 import (
-	"io/ioutil"
+	"bytes"
+	"io"
 	"os"
 	"path/filepath"
+	"strings"
 	"testing"
 
 	"github.com/asticode/go-astiav"
 	"github.com/stretchr/testify/require"
 )
 
-func TestIOContext(t *testing.T) {
+func TestIOContext_Write(t *testing.T) {
+	ioctxWrapper(t, func(c *astiav.IOContext, path string) {
+		err := c.Open(path, astiav.NewIOContextFlags(
+			astiav.IOContextFlagWrite))
+		require.NoError(t, err)
+
+		n, err := c.Write(nil)
+		require.NoError(t, err)
+		require.Equal(t, 0, n)
+
+		l, err := c.Write([]byte("test"))
+		require.Equal(t, 4, l)
+		require.NoError(t, err)
+
+		err = c.Closep()
+		require.NoError(t, err)
+
+		b, err := os.ReadFile(path)
+		require.NoError(t, err)
+		require.Equal(t, "test", string(b))
+	})
+}
+
+func TestIOContext_WriteCopy(t *testing.T) {
+	ioctxWrapper(t, func(c *astiav.IOContext, path string) {
+		err := c.Open(path, astiav.NewIOContextFlags(
+			astiav.IOContextFlagWrite))
+		require.NoError(t, err)
+
+		n, err := c.Write(nil)
+		require.NoError(t, err)
+		require.Equal(t, 0, n)
+
+		l, err := io.Copy(c, strings.NewReader("test"))
+		require.Equal(t, int64(4), l)
+		require.NoError(t, err)
+
+		err = c.Closep()
+		require.NoError(t, err)
+
+		b, err := os.ReadFile(path)
+		require.NoError(t, err)
+		require.Equal(t, "test", string(b))
+	})
+}
+
+func TestIOContext_Read(t *testing.T) {
+	ioctxWrapper(t, func(c *astiav.IOContext, path string) {
+		err := os.WriteFile(path, []byte("testing"), 0644)
+		require.NoError(t, err)
+
+		err = c.Open(path, astiav.NewIOContextFlags(
+			astiav.IOContextFlagRead))
+		require.NoError(t, err)
+
+		// Read from file (using astiav.IOContext.Read())
+		d := make([]byte, 16)
+		n, err := c.Read(d)
+		require.NoError(t, err)
+		require.Equal(t, 7, n)
+		require.Equal(t, "testing", string(d[:n]))
+
+		// Close context
+		err = c.Closep()
+		require.NoError(t, err)
+	})
+}
+
+func TestIOContext_ReadCopy(t *testing.T) {
+	ioctxWrapper(t, func(c *astiav.IOContext, path string) {
+		err := os.WriteFile(path, []byte("testing"), 0644)
+		require.NoError(t, err)
+
+		err = c.Open(path, astiav.NewIOContextFlags(
+			astiav.IOContextFlagRead))
+		require.NoError(t, err)
+
+		// Read from file (using astiav.IOContext.Read())
+		buf := bytes.NewBufferString("")
+		n, err := io.Copy(buf, c)
+		require.Equal(t, int64(7), n)
+		require.NoError(t, err)
+		require.Equal(t, "testing", buf.String())
+
+		err = c.Closep()
+		require.NoError(t, err)
+	})
+}
+
+func TestIOSeekerContext_Seek(t *testing.T) {
+	ioctxWrapper(t, func(c *astiav.IOContext, path string) {
+		err := os.WriteFile(path, []byte("testing"), 0644)
+		require.NoError(t, err)
+
+		err = c.Open(path, astiav.NewIOContextFlags(
+			astiav.IOContextFlagRead))
+		require.NoError(t, err)
+
+		l, err := c.Seek(4, io.SeekStart)
+		require.Equal(t, int64(4), l)
+		require.NoError(t, err)
+
+		d := make([]byte, 16)
+		n, err := c.Read(d)
+		require.Equal(t, 3, n)
+		require.NoError(t, err)
+		require.Equal(t, "ing", string(d[:n]))
+
+		err = c.Closep()
+		require.NoError(t, err)
+	})
+}
+
+func ioctxWrapper(t *testing.T, cb func(c *astiav.IOContext, path string)) {
 	c := astiav.NewIOContext()
 	path := filepath.Join(t.TempDir(), "iocontext.txt")
-	err := c.Open(path, astiav.NewIOContextFlags(astiav.IOContextFlagWrite))
-	require.NoError(t, err)
-	c.Write(nil)
-	c.Write([]byte("test"))
-	err = c.Closep()
-	require.NoError(t, err)
-	b, err := ioutil.ReadFile(path)
-	require.NoError(t, err)
-	require.Equal(t, "test", string(b))
-	err = os.Remove(path)
+
+	cb(c, path)
+
+	err := os.Remove(path)
 	require.NoError(t, err)
 }
