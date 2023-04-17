2a42f306dcb5613ea1703c6ebef610febddb8ca7
tumb1er <zimbler@gmail.com>
2023-04-17 14:08:10 +0300

    fix new device context

--- device_context.go
+++ device_context.go
@@ -6,7 +6,7 @@ import "C"
 
 func NewDeviceContext(deviceType DeviceTypeFlag) *BufferRef {
 	br := newBufferRef()
-	ret := int(C.av_hwdevice_ctx_create(&br.c, deviceType, nil, nil, 0))
+	ret := int(C.av_hwdevice_ctx_create(&br.c, uint32(deviceType), nil, nil, 0))
 	if ret < 0 {
 		return nil
 	} else {
