dc7680db88ebd8344b3fabef7912e8bc1f721f66
tumb1er <zimbler@gmail.com>
2023-04-17 13:38:29 +0300

    av_buffer_ref wrapper

---
+++ bufferref.go
@@ -0,0 +1,25 @@
+package astiav
+
+// #cgo pkg-config: libavutil
+// #include <libavutil/buffer.h>
+import "C"
+
+type BufferRef struct {
+	c *C.struct_AVBufferRef
+}
+
+func newBufferRef() *BufferRef {
+	return &BufferRef{}
+}
+
+func newBufferFromC(c *C.struct_AVBufferRef) *BufferRef {
+	if c == nil {
+		return nil
+	}
+	return &BufferRef{c: c}
+}
+
+func (br *BufferRef) Ref() *BufferRef {
+	c := C.av_buffer_ref(br.c)
+	return newBufferFromC(c)
+}
