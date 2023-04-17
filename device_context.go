22674d08faaf9ac7b1b310df6820753f8c95db98
tumb1er <zimbler@gmail.com>
2023-04-17 13:38:57 +0300

    NewDeviceContext

---
+++ device_context.go
@@ -0,0 +1,15 @@
+package astiav
+
+// #cgo pkg-config: libavutil
+// #include <libavutil/hwcontext.h>
+import "C"
+
+func NewDeviceContext(deviceType DeviceTypeFlag) *BufferRef {
+	br := newBufferRef()
+	ret := int(C.av_hwdevice_ctx_create(&br.c, deviceType, nil, nil, 0))
+	if ret < 0 {
+		return nil
+	} else {
+		return br
+	}
+}
