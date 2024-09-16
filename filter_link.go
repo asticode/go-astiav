fba33b1e080c3e4462b5c2dbbf28697e8bc269e9
Quentin Renard <contact-github@asticode.com>
2024-09-16 16:43:38 +0200

    Added getters to FilterLink

--- filter_link.go
+++ filter_link.go
@@ -15,6 +15,43 @@ func newFilterLinkFromC(c *C.AVFilterLink) *FilterLink {
 	return &FilterLink{c: c}
 }
 
+func (l *FilterLink) ChannelLayout() ChannelLayout {
+	v, _ := newChannelLayoutFromC(&l.c.ch_layout).clone()
+	return v
+}
+
+func (l *FilterLink) FrameRate() Rational {
+	return newRationalFromC(l.c.frame_rate)
+}
+
+func (l *FilterLink) Height() int {
+	return int(l.c.h)
+}
+
+func (l *FilterLink) MediaType() MediaType {
+	return MediaType(l.c._type)
+}
+
+func (l *FilterLink) PixelFormat() PixelFormat {
+	return PixelFormat(l.c.format)
+}
+
+func (l *FilterLink) SampleAspectRatio() Rational {
+	return newRationalFromC(l.c.sample_aspect_ratio)
+}
+
+func (l *FilterLink) SampleFormat() SampleFormat {
+	return SampleFormat(l.c.format)
+}
+
+func (l *FilterLink) SampleRate() int {
+	return int(l.c.sample_rate)
+}
+
 func (l *FilterLink) TimeBase() Rational {
 	return newRationalFromC(l.c.time_base)
+}
+
+func (l *FilterLink) Width() int {
+	return int(l.c.w)
 }
