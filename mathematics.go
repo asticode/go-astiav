33c7b13fefab0d8b13b1a051e1031acf64c3c8e5
ma3315865 <ma3315865@outlook.com>
2024-12-03 17:12:32 +0800

    remove q2d

--- mathematics.go
+++ mathematics.go
@@ -12,8 +12,3 @@ func RescaleQ(a int64, b Rational, c Rational) int64 {
 func RescaleQRnd(a int64, b Rational, c Rational, r Rounding) int64 {
 	return int64(C.av_rescale_q_rnd(C.int64_t(a), b.c, c.c, C.enum_AVRounding(r)))
 }
-
-// https://ffmpeg.org/doxygen/7.0/group__lavu__math__rational.html#ga935dbbf6bde8dfe5fa7ddb1da582eb07
-func Q2D(a Rational) float64 {
-	return float64(C.av_q2d(a.c))
-}
--- mathematics_test.go
+++ mathematics_test.go
@@ -10,7 +10,4 @@ func TestMathematics(t *testing.T) {
 	require.Equal(t, int64(1000), RescaleQ(100, NewRational(1, 100), NewRational(1, 1000)))
 	require.Equal(t, int64(0), RescaleQRnd(1, NewRational(1, 100), NewRational(1, 10), RoundingDown))
 	require.Equal(t, int64(1), RescaleQRnd(1, NewRational(1, 100), NewRational(1, 10), RoundingUp))
-	require.Equal(t, 0.04, Q2D(NewRational(1, 25)))
-	require.Equal(t, 0.3, Q2D(NewRational(3, 10)))
-	require.Equal(t, float64(30), Q2D(NewRational(30, 1)))
 }
