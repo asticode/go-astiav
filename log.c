3a54564426861b3728db9ce85de5d70c4383bf24
Quentin Renard <contact-github@asticode.com>
2026-04-01 09:59:51 +0000

    Added ASTIAV_LOG_BUF_SIZE C constant to avoid log callback vsprintf overflow

--- log.c
+++ log.c
@@ -2,10 +2,14 @@
 #include <libavutil/log.h>
 #include <stdio.h>
 
+#ifndef ASTIAV_LOG_BUF_SIZE
+#define ASTIAV_LOG_BUF_SIZE 4096
+#endif
+
 void astiavLogCallback(void *ptr, int level, const char *fmt, va_list vl)
 {
 	if (level > av_log_get_level()) return;
-	char msg[1024];
+	char msg[ASTIAV_LOG_BUF_SIZE];
 	vsprintf(msg, fmt, vl);
 	goAstiavLogCallback(ptr, level, (char*)(fmt), msg);
 }
